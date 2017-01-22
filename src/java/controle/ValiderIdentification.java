ad01bd62f3b98eb16421b76ff75c893999b1c5a9
Yodaii <yohann.vaubourg@gmail.com>
2017-01-22 18:31:50 +0100

    identification basique

--- src/java/controle/ValiderIdentification.java
+++ src/java/controle/ValiderIdentification.java
@@ -4,12 +4,20 @@
  */
 package controle;
 
+import static com.opensymphony.xwork2.Action.INPUT;
 import com.opensymphony.xwork2.ActionSupport;
+import java.util.Map;
+import objet_metiers.Abonne;
+import org.apache.struts2.interceptor.SessionAware;
+import org.hibernate.Session;
+import util.HibernateUtil;
 
-public class ValiderIdentification extends ActionSupport{
-    
+public class ValiderIdentification extends ActionSupport implements SessionAware {
+
     private String identifiant;
     private String motdepasse;
+    private Map<String, Object> session;
+    private Session uneSession;
 
     public String getIdentifiant() {
         return identifiant;
@@ -26,8 +34,21 @@ public class ValiderIdentification extends ActionSupport{
     public void setMotdepasse(String motdepasse) {
         this.motdepasse = motdepasse;
     }
-    
+
     public String execute() throws Exception {
+        uneSession = HibernateUtil.currentSession();
+        Abonne unAbon = (Abonne) uneSession.get(Abonne.class, identifiant);
+        if (unAbon == null || !unAbon.getMotdepasse().equals(motdepasse)) {
+            addActionError("L'abonné n'existe pas !");
+            return INPUT;
+        }
+        session.put("unAbon", unAbon);
+        session.put("session", uneSession);
         return SUCCESS;
+    }
+
+    @Override
+    public void setSession(Map<String, Object> session) {
+        this.session = session;
     }
 }
